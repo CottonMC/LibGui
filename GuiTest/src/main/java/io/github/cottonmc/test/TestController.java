6134cc78ea2452e9d133258dfcf5bc634148d576
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-05-08 20:21:55 +0300

    Update test mod to 20w17a

--- GuiTest/src/main/java/io/github/cottonmc/test/TestController.java
+++ GuiTest/src/main/java/io/github/cottonmc/test/TestController.java
@@ -1,18 +1,18 @@
 package io.github.cottonmc.test;
 
-import io.github.cottonmc.cotton.gui.CottonCraftingController;
+import io.github.cottonmc.cotton.gui.CottonInventoryController;
 import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WItemSlot;
 import io.github.cottonmc.cotton.gui.widget.WPlayerInvPanel;
-import net.minecraft.container.BlockContext;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.text.LiteralText;
 
-public class TestContainer extends CottonCraftingController {
+public class TestController extends CottonInventoryController {
 	
-	public TestContainer(int syncId, PlayerInventory playerInventory, BlockContext context) {
-		super(null, syncId, playerInventory, getBlockInventory(context), null);
+	public TestController(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+		super(syncId, playerInventory, getBlockInventory(context), null);
 		
 		WGridPanel root = (WGridPanel)this.getRootPanel();
 		
