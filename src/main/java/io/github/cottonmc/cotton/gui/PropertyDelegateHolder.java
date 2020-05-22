58cbdd972d1d5ac403ec039f447c1ff66211ece2
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-05-22 11:57:43 +0300

    Document PropertyDelegateHolder

--- src/main/java/io/github/cottonmc/cotton/gui/PropertyDelegateHolder.java
+++ src/main/java/io/github/cottonmc/cotton/gui/PropertyDelegateHolder.java
@@ -2,6 +2,20 @@ package io.github.cottonmc.cotton.gui;
 
 import net.minecraft.screen.PropertyDelegate;
 
+/**
+ * This interface can be implemented on block entity classes
+ * for providing a property delegate.
+ *
+ * @see CottonInventoryController#getBlockPropertyDelegate(net.minecraft.screen.ScreenHandlerContext)
+ */
 public interface PropertyDelegateHolder {
+	/**
+	 * Gets this block entity's property delegate.
+	 *
+	 * <p>On the client, the returned property delegate <b>must</b> have a working implementation of
+	 * {@link PropertyDelegate#set(int, int)}.
+	 *
+	 * @return the property delegate
+	 */
 	public PropertyDelegate getPropertyDelegate();
 }
