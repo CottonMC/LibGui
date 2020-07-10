e7d6b2f86425be7e4af511a4035f206e591a82f7
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-07-11 00:40:46 +0300

    Add tab titles and switch to a builder model for adding tabs

--- src/main/java/io/github/cottonmc/cotton/gui/widget/WTabPanel.java
+++ src/main/java/io/github/cottonmc/cotton/gui/widget/WTabPanel.java
@@ -2,19 +2,26 @@ package io.github.cottonmc.cotton.gui.widget;
 
 import com.google.common.collect.ImmutableList;
 import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
+import io.github.cottonmc.cotton.gui.client.LibGuiClient;
+import io.github.cottonmc.cotton.gui.client.ScreenDrawing;
 import io.github.cottonmc.cotton.gui.widget.data.Axis;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.icon.Icon;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.StringRenderable;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * A panel that contains creative inventory-style tabs on the top.
@@ -63,6 +70,18 @@ public class WTabPanel extends WPanel {
 		mainPanel.add(tab.getWidget());
 	}
 
+	/**
+	 * Configures and adds a tab to this panel.
+	 *
+	 * @param widget       the contained widget
+	 * @param configurator the tab configurator
+	 */
+	public void add(WWidget widget, Consumer<Tab.Builder> configurator) {
+		Tab.Builder builder = new Tab.Builder(widget);
+		configurator.accept(builder);
+		add(builder.build());
+	}
+
 	@Override
 	public void setSize(int x, int y) {
 		super.setSize(x, y);
@@ -80,52 +99,50 @@ public class WTabPanel extends WPanel {
 	 * The data of a tab.
 	 */
 	public static class Tab {
+		@Nullable
+		private final StringRenderable title;
+		@Nullable
 		private final Icon icon;
 		private final WWidget widget;
 		private final List<StringRenderable> tooltip;
 
 		/**
-		 * Constructs a tab with no tooltip.
-		 *
-		 * @param icon   the tab icon
-		 * @param widget the widget contained in the tab
-		 * @throws NullPointerException if any parameter is null
-		 */
-		public Tab(Icon icon, WWidget widget) {
-			this(icon, widget, ImmutableList.of());
-		}
-
-		/**
 		 * Constructs a tab.
 		 *
-		 * @param icon    the tab icon
-		 * @param widget  the widget contained in the tab
-		 * @param tooltip the tab tooltip
-		 * @throws NullPointerException if any parameter is null
-		 */
-		public Tab(Icon icon, WWidget widget, StringRenderable tooltip) {
-			this(icon, widget, ImmutableList.of(Objects.requireNonNull(tooltip, "tooltip")));
-		}
-
-		/**
-		 * Constructs a tab.
-		 *
+		 * @param title   the tab title
 		 * @param icon    the tab icon
 		 * @param widget  the widget contained in the tab
 		 * @param tooltip the tab tooltip lines
-		 * @throws NullPointerException if any parameter is null
+		 * @throws IllegalArgumentException if both the title and the icon are null
+		 * @throws NullPointerException     if the widget or the tooltip list are null
 		 */
-		public Tab(Icon icon, WWidget widget, List<StringRenderable> tooltip) {
-			this.icon = Objects.requireNonNull(icon, "icon");
+		public Tab(@Nullable StringRenderable title, @Nullable Icon icon, WWidget widget, List<StringRenderable> tooltip) {
+			if (title == null && icon == null) {
+				throw new IllegalArgumentException("A tab must have a title or an icon");
+			}
+
+			this.title = title;
+			this.icon = icon;
 			this.widget = Objects.requireNonNull(widget, "widget");
 			this.tooltip = ImmutableList.copyOf(Objects.requireNonNull(tooltip, "tooltip"));
 		}
 
 		/**
+		 * Gets the title of this tab.
+		 *
+		 * @return the title, or null if there's no title
+		 */
+		@Nullable
+		public StringRenderable getTitle() {
+			return title;
+		}
+
+		/**
 		 * Gets the icon of this tab.
 		 *
-		 * @return the icon
+		 * @return the icon, or null if there's no title
 		 */
+		@Nullable
 		public Icon getIcon() {
 			return icon;
 		}
@@ -147,13 +164,56 @@ public class WTabPanel extends WPanel {
 		public void addTooltip(List<StringRenderable> tooltip) {
 			tooltip.addAll(this.tooltip);
 		}
+
+		public static final class Builder {
+			@Nullable
+			private StringRenderable title;
+			@Nullable
+			private Icon icon;
+			private final WWidget widget;
+			private final List<StringRenderable> tooltip = new ArrayList<>();
+
+			private Builder(WWidget widget) {
+				this.widget = Objects.requireNonNull(widget, "widget");
+			}
+
+			public Builder title(StringRenderable title) {
+				this.title = Objects.requireNonNull(title, "title");
+				return this;
+			}
+
+			public Builder icon(Icon icon) {
+				this.icon = Objects.requireNonNull(icon, "icon");
+				return this;
+			}
+
+			public Builder tooltip(StringRenderable... lines) {
+				Objects.requireNonNull(lines, "lines");
+
+				for (StringRenderable line : lines) {
+					tooltip.add(line);
+				}
+
+				return this;
+			}
+
+			public Builder tooltip(Collection<? extends StringRenderable> lines) {
+				Objects.requireNonNull(lines, "lines");
+				tooltip.addAll(lines);
+				return this;
+			}
+
+			public Tab build() {
+				return new Tab(title, icon, widget, tooltip);
+			}
+		}
 	}
 
 	private final class WTab extends WWidget {
 		private final Tab data;
 		boolean selected = false;
 
-		public WTab(Tab data) {
+		WTab(Tab data) {
 			this.data = data;
 		}
 
@@ -175,13 +235,47 @@ public class WTabPanel extends WPanel {
 		@Environment(EnvType.CLIENT)
 		@Override
 		public void paint(MatrixStack matrices, int x, int y, int mouseX, int mouseY) {
+			TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
+			StringRenderable title = data.getTitle();
+			Icon icon = data.getIcon();
+
+			if (title != null) {
+				int width = TAB_WIDTH + renderer.getWidth(title);
+				if (icon == null) width = Math.max(TAB_WIDTH, width - ICON_SIZE);
+
+				if (this.width != width) {
+					setSize(width, this.height);
+					getParent().layout();
+				}
+			}
+
 			if (selected) {
 				BackgroundPainter.SELECTED_TAB.paintBackground(x, y, this);
 			} else {
 				BackgroundPainter.UNSELECTED_TAB.paintBackground(x, y, this);
 			}
 
-			data.getIcon().paint(matrices, x + (width - ICON_SIZE) / 2, y + (height - TAB_PADDING - ICON_SIZE) / 2, ICON_SIZE);
+			int iconX = 6;
+
+			if (title != null) {
+				int titleX = (icon != null) ? iconX + ICON_SIZE + 1 : 0;
+				int titleY = (height - TAB_PADDING - renderer.fontHeight) / 2 + 1;
+				int width = (icon != null) ? this.width - iconX - ICON_SIZE : this.width;
+				HorizontalAlignment align = (icon != null) ? HorizontalAlignment.LEFT : HorizontalAlignment.CENTER;
+
+				int color;
+				if (LibGuiClient.config.darkMode) {
+					color = WLabel.DEFAULT_DARKMODE_TEXT_COLOR;
+				} else {
+					color = selected ? WLabel.DEFAULT_TEXT_COLOR : 0xEEEEEE;
+				}
+
+				ScreenDrawing.drawString(matrices, title, align, x + titleX, y + titleY, width, color);
+			}
+
+			if (icon != null) {
+				icon.paint(matrices, x + iconX, y + (height - TAB_PADDING - ICON_SIZE) / 2, ICON_SIZE);
+			}
 		}
 
 		@Override
