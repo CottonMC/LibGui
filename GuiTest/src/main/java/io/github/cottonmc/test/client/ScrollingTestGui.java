fdaa5276124335b02ce5631197661f2aa90d165f
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-06-04 15:55:57 +0300

    Add test title to ScrollingTestGui

--- GuiTest/src/main/java/io/github/cottonmc/test/client/ScrollingTestGui.java
+++ GuiTest/src/main/java/io/github/cottonmc/test/client/ScrollingTestGui.java
@@ -7,9 +7,11 @@ import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WBox;
 import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WLabel;
 import io.github.cottonmc.cotton.gui.widget.WLabeledSlider;
 import io.github.cottonmc.cotton.gui.widget.WScrollPanel;
 import io.github.cottonmc.cotton.gui.widget.data.Axis;
+import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
 import io.github.cottonmc.cotton.gui.widget.icon.ItemIcon;
 
 public class ScrollingTestGui extends LightweightGuiDescription {
@@ -23,7 +25,8 @@ public class ScrollingTestGui extends LightweightGuiDescription {
 
 		box.add(new WButton(new ItemIcon(Items.APPLE)));
 
-		root.add(new WScrollPanel(box), 0, 0, 5, 3);
+		root.add(new WLabel(new LiteralText("Scrolling test")).setVerticalAlignment(VerticalAlignment.CENTER), 0, 0, 5, 2);
+		root.add(new WScrollPanel(box), 0, 2, 5, 3);
 		root.validate(this);
 	}
 }
