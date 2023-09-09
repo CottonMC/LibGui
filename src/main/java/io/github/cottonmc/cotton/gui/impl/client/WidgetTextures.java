8d1d2442866b3bb3a4b1d098add0ccb65343cd60
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-09-09 22:19:41 +0300

    Move scroll bar textures to GUI sprites

--- src/main/java/io/github/cottonmc/cotton/gui/impl/client/WidgetTextures.java
+++ src/main/java/io/github/cottonmc/cotton/gui/impl/client/WidgetTextures.java
@@ -22,6 +22,18 @@ public final class WidgetTextures {
 		id("widget/button_disabled_dark"),
 		id("widget/button_highlighted_dark")
 	);
+	private static final ScrollBarTextures LIGHT_SCROLL_BAR = new ScrollBarTextures(
+		id("widget/scroll_bar/background_light"),
+		id("widget/scroll_bar/thumb_light"),
+		id("widget/scroll_bar/thumb_pressed_light"),
+		id("widget/scroll_bar/thumb_hovered_light")
+	);
+	private static final ScrollBarTextures DARK_SCROLL_BAR = new ScrollBarTextures(
+		id("widget/scroll_bar/background_dark"),
+		id("widget/scroll_bar/thumb_dark"),
+		id("widget/scroll_bar/thumb_pressed_dark"),
+		id("widget/scroll_bar/thumb_hovered_dark")
+	);
 
 	public static ButtonTextures getButtonTextures(boolean dark) {
 		return dark ? DARK_BUTTON : PressableWidgetAccessor.libgui$getTextures();
@@ -31,7 +43,14 @@ public final class WidgetTextures {
 		return dark ? DARK_LABELED_SLIDER_HANDLE : LIGHT_LABELED_SLIDER_HANDLE;
 	}
 
+	public static ScrollBarTextures getScrollBarTextures(boolean dark) {
+		return dark ? DARK_SCROLL_BAR : LIGHT_SCROLL_BAR;
+	}
+
 	private static Identifier id(String path) {
 		return new Identifier(LibGuiCommon.MOD_ID, path);
+	}
+
+	public record ScrollBarTextures(Identifier background, Identifier thumb, Identifier thumbPressed, Identifier thumbHovered) {
 	}
 }
